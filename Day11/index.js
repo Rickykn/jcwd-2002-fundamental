0d11f45e2e0e0d062ceab58ed4fef98266095f42
rickykurniawan350 <rickykurniawan350@gmail.com>
2022-02-22 01:10:00 +0700

    latihan reverse

--- Day11/index.js
+++ Day11/index.js
@@ -1,25 +1,38 @@
-const number = [1, 2, 3, 4, 5, 6, 7, 8];
+// const number = [1, 2, 3, 4, 5, 6, 7, 8];
 
-// const duplicateForEach = (arr, callback) => {
+// // const duplicateForEach = (arr, callback) => {
+// //   for (let i = 0; i < arr.length; i++) {
+// //     callback(arr[i], i, arr);
+// //   }
+// // };
+
+// // duplicateForEach(number, (val,idx,arr) => {
+
+// // });
+
+// const duplicateFilter = (arr, callback) => {
+//   let resultArr = [];
 //   for (let i = 0; i < arr.length; i++) {
-//     callback(arr[i], i, arr);
+//     if (callback(arr[i], i, arr)) {
+//       resultArr.push(arr[i]);
+//     }
 //   }
+//   return resultArr;
 // };
 
-// duplicateForEach(number, (val,idx,arr) => {
+// let result = duplicateFilter(number, (val) => val > 6);
 
-// });
+// console.log(result);
 
-const duplicateFilter = (arr, callback) => {
-  let resultArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (callback(arr[i], i, arr)) {
-      resultArr.push(arr[i]);
-    }
+var reverseString = function(s) {
+  let result = ""
+  
+  for(let i=s.length-1;i>=0;i--){
+      result += s[i]
   }
-  return resultArr;
+  return result.split("")
 };
 
-let result = duplicateFilter(number, (val) => val > 6);
 
-console.log(result);
+
+console.log(reverseString([ 'h', 'e', 'l', 'l', 'o' ]))
